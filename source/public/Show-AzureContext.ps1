eb1b20716189df2576d6460994f418bcf58d11f4
Joel Bennett <Jaykul@HuddledMasses.org>
2025-07-05 16:54:38 -0400

    Add Show-AzureContext -SubscriptionNames
    A subscription ID  lookup for short subscription names

--- source/public/Show-AzureContext.ps1
+++ source/public/Show-AzureContext.ps1
@@ -2,8 +2,11 @@ function Show-AzureContext {
     [Alias("AzureContextBlock","New-AzureContextBlock")]
     [CmdletBinding()]
     param(
-        # A string to show before the output. Defaults to "$fg:32aee7&nf-md-microsoft_azure;$fg:clear"
-        $Prefix = "$fg:32aee7&nf-md-microsoft_azure;$fg:clear",
+        # A string to show before the output. Defaults to "${fg:32aee7}${nf:md-microsoft_azure} ${fg:clear}"
+        $Prefix = "$fg:32aee7${nf:md-microsoft_azure} ${fg:clear}",
+
+        # A hashtable of Subscription Ids to the desired text to show.
+        [hashtable]$SubscriptionNames,
 
         # Force imports the module if it's not imported
         # By default, this block only renders when Az.Accounts is imported.
@@ -11,7 +14,11 @@ function Show-AzureContext {
     )
     if ($Force -or (Get-Module Az.Accounts)) {
         if (($Context = Get-AzContext)) {
-            $Context.Name
+            if ($SubscriptionNames -and $SubscriptionNames.ContainsKey($Context.Subscription.Id)) {
+                $SubscriptionNames[$Context.Subscription.Id]
+            } else {
+                $Context.Name
+            }
         }
     }
 }
